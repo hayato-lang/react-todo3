c639e7647124af6028530248f34f20ced2b0e6ae
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-11-08 15:32:07 +0900

    state

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
 export const App = () => {
-  const onClickButton = () => {
-    alert("戻す");
+  const [num, setNum] = useState(0);
+  const onClickCountUp = () => {
+    setNum(num + 1);
+    if (num % 3 === 0) {
+    }
   };
   return (
     <>
       <h1 style={{ color: "blue" }}>五条悟です</h1>
       <ColorfulMessage color="red">虎杖です</ColorfulMessage>
       <ColorfulMessage color="pink">釘崎です</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
